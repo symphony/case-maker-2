74908b9b639eabea6873cd4af49cab71f90a4caa
symphony <jciurc@gmail.com>
2022-04-17 23:12:40 -0600

    completed a few cases

---
+++ assertEqual.js
@@ -0,0 +1,30 @@
+const eqNonObj = function(elemA, elemB) {
+  if (elemA === elemB) return true;
+  if (!Array.isArray(elemA) || !Array.isArray(elemB)) return false;
+  if (elemA.length !== elemB.length) return false;
+  for (let i = 0; i < elemA.length; i++) {
+    if (elemA[i] !== elemB[i]) return false;
+  }
+  return true;
+};
+
+const eqObjects = function(objectA, objectB) {
+  if (Array.isArray(objectA) || Array.isArray(objectB)) return false;
+  if (Object.keys(objectA).length !== Object.keys(objectB).length) return false;
+  for (const key in objectA) {
+    if (!eqNonObj(objectA[key], objectB[key])) return false;
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  const failedMsg = `🦐 Assertion Failed 🍅 > ${inspect(actual)} !== ${inspect(expected)}`;
+  const passedMsg = `👽 Assertion Passed 🐸 > ${inspect(actual)} === ${inspect(expected)}`;
+  let isEqual = eqNonObj(actual, expected); // send to sub function
+  const areObjs = typeof(actual) === "object" && typeof(expected) === "object";
+  if (!isEqual && areObjs) isEqual = eqObjects(actual, expected); // send to sub function
+  console.log(isEqual ? passedMsg : failedMsg);
+};
+
+module.exports = { assertEqual };
--- case-maker-2.js
+++ case-maker-2.js
@@ -1,43 +1,45 @@
+const { assertEqual } = require('./assertEqual');
+
 const makeCase = (input, style) => {
-  const priority = ['camel', 'pascal', 'snake', 'kebab', 'title', 'vowel', 'consonant', 'upper', 'lower'];
+  const priority = [ 'upper', 'lower', 'vowel', 'consonant', 'camel', 'pascal', 'snake', 'kebab', 'title' ];
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
   const cases = {
-    camel: (string) => {
+    camel: (phrase) => {
 
     },
-    pascal: (string) => {
+    pascal: (phrase) => {
+      return phrase.map(a => a.slice(0, 1).toUpperCase() + a.slice(1)).join('');
+    },
+    snake: (phrase) => {
+      return phrase.join('_');
+    },
+    kebab: (phrase) => {
+      return phrase.join('-');
+    },
+    title: (phrase) => {
+      return phrase.map(a => a.slice(0, 1).toUpperCase() + a.slice(1)).join(' ');
+    },
+    vowel: (phrase) => {
 
     },
-    snake: (string) => {
+    consonant: (phrase) => {
 
     },
-    kebab: (string) => {
-
-    },
-    title: (string) => {
-
-    },
-    vowel: (string) => {
-
-    },
-    consonant: (string) => {
-
-    },
-    upper: (string) => {
-
-    },
-    lower: (string) => {
-
+    upper: (phrase) => {
+      return phrase.join('_').toUpperCase();
     },
   };
+  // Sanitize inputs
+  const inputX = input.trim().toLowerCase().split(/[\s-_+]/);
   const styleX = Array.isArray(style) ? priority.find(a => style.includes(a)) : style;
-  return cases[styleX](input);
+  return cases[styleX](inputX);
 };
 
-console.log(makeCase("this is a string", "camel")); // thisIsAString
-console.log(makeCase("this is a string", "pascal")); // ThisIsAString
-console.log(makeCase("this is a string", "snake")); // this_is_a_string
-console.log(makeCase("this is a string", "kebab")); // this-is-a-string
-console.log(makeCase("this is a string", "title")); // This Is A String
-console.log(makeCase("this is a string", "vowel")); // thIs Is A strIng
-console.log(makeCase("this is a string", "consonant")); // THiS iS a STRiNG
-console.log(makeCase("this is a string", ["upper", "snake"])); // THIS_IS_A_STRING
+assertEqual(makeCase("this-is-a string", "camel"), 'thisIsAString');
+assertEqual(makeCase("this is a string", "pascal"), 'ThisIsAString');
+assertEqual(makeCase("tHis is a+string ", "snake"), 'this_is_a_string');
+assertEqual(makeCase("this is a stRing", "kebab"), 'this-is-a-string');
+assertEqual(makeCase(" this is_a string", "title"), 'This Is A String');
+assertEqual(makeCase("this iS a string", "vowel"), 'thIs Is A strIng');
+assertEqual(makeCase("this is a string", "consonant"), 'THiS iS a STRiNG');
+assertEqual(makeCase("thIs is a string", ["upper", "snake"]), 'THIS_IS_A_STRING');
